1f7a429fb63cab930f0b4750f09338ddeec8b396
Emircan Dalman <emircandalman28@gmail.com>
2021-03-09 01:33:27 +0300

    Şart Blokları

---
+++ Conditional/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Conditional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isLogin = false;
+            if (isLogin )
+            {
+                Console.WriteLine("Giriş Başarılı");
+            }
+            else
+            {
+                Console.WriteLine("Giriş Başarısız");
+            }
+
+            var number = 20;
+            if (number > 10)
+            {
+                Console.WriteLine("Sayı 10'dan büyük");
+            }
+            else if (number < 15)
+            {
+                Console.WriteLine("Sayı 15'den küçük");
+            }
+            else if (number < 20)
+            {
+                Console.WriteLine("Sayı 20'den küçük");
+            }
+            else
+            {
+                Console.WriteLine("Sayı 20'den büyük");
+            }
+        }
+    }
+}
