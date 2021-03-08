900b4380c5f6bc9bea2ecc33a545b660a7bbbe6b
Emircan Dalman <emircandalman28@gmail.com>
2021-03-09 01:08:10 +0300

    Add project files.

---
+++ TypesAndVariables/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TypesAndVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ReSharper
+            //Variables - Değişkenler
+            //int - Integer
+            //Camel Case - Deve Yazımı
+
+            int number1 = 20;
+            Console.WriteLine("Sayı1: "+number1);
+
+            double number2 = 20.5;
+            Console.WriteLine("Sayı2: {0}", number2);
+
+            bool isLogin = true;
+
+            var number3 = true;
+
+            //Enum 
+        }
+    }
+}
