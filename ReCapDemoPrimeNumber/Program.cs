259bd05ea4687c3ab8223ba883b7f5f5c57445c3
Emircan Dalman <emircandalman28@gmail.com>
2021-03-09 18:01:11 +0300

    Temel C# Kursu Bitirildi

---
+++ ReCapDemoPrimeNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ReCapDemoPrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Sayıyı giriniz: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(IsPrime(number) 
+                ? "Sayı Asaldır" 
+                : "Sayı asal değildir");
+        }
+
+        static bool IsPrime(int number)
+        {
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
