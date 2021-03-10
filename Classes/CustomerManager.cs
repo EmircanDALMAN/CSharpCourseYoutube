520ae42f73792b9fa03619efea37c0ab029805f8
Emircan Dalman <emircandalman28@gmail.com>
2021-03-10 19:11:46 +0300

    Class'lara Giriş Yaptı

---
+++ Classes/CustomerManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes
+{
+    class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine(customer.FirstName + " "+ customer.LastName + " Eklendi");
+        }
+
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine(customer.FirstName + " " + customer.LastName + " Silindi");
+        }
+    }
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Customer customer = new Customer
+            {
+                Id = 1,
+                FirstName = "Emircan",
+                LastName = "Dalman"
+            };
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer);
+            customerManager.Delete(customer);
+
+        }
+    }
+}
