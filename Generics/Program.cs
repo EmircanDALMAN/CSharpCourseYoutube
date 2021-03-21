5e093dc7429d8973afc5bef89a7a58f3675157e8
Emircan Dalman <emircandalman28@gmail.com>
2021-03-21 22:43:26 +0300

    Generic'ler anlatıldı

---
+++ Generics/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    interface IAppRepository<T> where T : class, IEntity, new()
+    {
+        void Add(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+        List<T> GetAll();
+    }
+
+    interface IProductService : IAppRepository<Product>
+    {
+
+    }
+
+    interface ICategoryService : IAppRepository<Category>
+    {
+
+    }
+
+    class ProductManager : IProductService
+    {
+        public void Add(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class CategoryManager : ICategoryService
+    {
+        public void Add(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    interface IEntity
+    {
+
+    }
+    class Product : IEntity
+    {
+
+    }
+
+    class Category : IEntity
+    {
+
+    }
+}
