2ac91a37333a6445122317b62998880d86bb12eb
Emircan Dalman <emircandalman28@gmail.com>
2021-03-26 17:13:19 +0300

    Attributeler ve Reflection'lar anlatıldı

---
+++ Attributes/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Attributes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            customer.Id = 1;
+            customer.FirstName = "Emircan";
+            CustomerDal customerDal = new CustomerDal();
+            customerDal.Add();
+        }
+    }
+    [ToTable("Customers")]
+    [ToTable("TblCustomers")]
+    public class Customer
+    {
+        public int Id { get; set; }
+        [RequiredProperty]
+        public string FirstName { get; set; }
+        [RequiredProperty]
+        public string LastName { get; set; }
+    }
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+    public class ToTableAttribute : Attribute
+    {
+        private string _tableName;
+        public ToTableAttribute(string tableName)
+        {
+            _tableName = tableName;
+        }
+    }
+    [AttributeUsage(AttributeTargets.Property)]
+    public class RequiredPropertyAttribute : Attribute
+    {
+
+    }
+    class CustomerDal
+    {
+        [Obsolete("Bu methodu kullanma, ben yeni ekleme methodu yazdım.")]
+        public void Add()
+        {
+            Console.WriteLine("Müşteri Eklendi");
+        }
+
+        public void Add2()
+        {
+            Console.WriteLine("Müşteri 2. methodla eklendi");
+        }
+    }
+}
---
+++ Reflection/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calculator calculator = new Calculator(2,4);
+            //Console.WriteLine(calculator.Sum());
+
+            //Console.WriteLine(calculator.GetType()
+            //    .GetMethod("Sum")
+            //    .Invoke(calculator, null));
+            //MethodInfo methodInfo = calculator.GetType().GetMethod("Sum");
+
+            //Console.WriteLine(methodInfo.Invoke(calculator, null));
+
+            var type = typeof(Calculator);
+
+            Calculator calculator =
+                (Calculator) Activator.CreateInstance(type, 5, 5);
+
+            var methods = type.GetMethods();
+            foreach (var methodInfo in methods)
+            {
+                Console.WriteLine("Method adı: {0}", methodInfo.Name);
+                foreach (var parameterInfo in methodInfo.GetParameters())
+                {
+                    Console.WriteLine("Parametre: {0}", parameterInfo.Name);
+                }
+
+                foreach (var attribute in methodInfo.GetCustomAttributes())
+                {
+                    Console.WriteLine("Attribute: {0}", attribute.GetType().Name);
+                }
+            }
+
+        }
+    }
+
+    public class Calculator
+    {
+        private int _number1;
+        private int _number2;
+        public Calculator(int number1, int number2)
+        {
+            _number1 = number1;
+            _number2 = number2;
+        }
+        [Custom("Toplama")]
+        public int Sum(int sayi1)
+        {
+            return _number1 + _number2;
+        }
+    }
+
+    public class CustomAttribute : Attribute
+    {
+        private string _name;
+
+        public CustomAttribute(string name)
+        {
+            _name = name;
+        }
+    }
+}
